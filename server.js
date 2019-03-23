ea08ce16399e31daf1abb9f3d9c8f672c03334ef
Brzeczyszczykiewicz <Samproschansky@gmail.com>
2019-03-23 12:50:49 -0400

    First heroku commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require('path');
+var PORT = 8080;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
